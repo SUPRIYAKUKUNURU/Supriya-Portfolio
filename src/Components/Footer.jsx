13cd7ba5d8bfcf700fa52a420dea990b34f411c5
SUPRIYA K <114737299+SUPRIYAKUKUNURU@users.noreply.github.com>
2025-11-15 18:14:39 +0530

    Update GitHub link in Footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <div className="text-grape text-center py-7">
             <p className='font-bold pb-2'>Crafted with ❤️ by Supriya Kukunuru</p>
             <a 
-            href="https://github.com/SUPRIYAKUKUNURU/Portfolio" 
+            href="https://github.com/SUPRIYAKUKUNURU" 
             target="_blank"
             rel="noopener noreferrer" 
             className="underline hover:opacity-70">View code on GitHub</a>
